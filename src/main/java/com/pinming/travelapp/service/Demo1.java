6efcaa805d0a22e320ead9aaeb45f1e2f961591a
xiangxiang <1296933286@qq.com>
2019-07-18 19:35:12 +0800

    添加了所有的层的名字-朱翔翔

--- src/main/java/com/pinming/travelapp/service/Demo1.java
+++ src/main/java/com/pinming/travelapp/service/Demo1.java
@@ -3,5 +3,6 @@ package com.pinming.travelapp.service;
 public class Demo1 {
     public static void main(String[] args) {
      System.out.println("5456打得开了吗啊");
+
     }
 }
---
+++ src/main/java/com/pinming/travelapp/service/impl/HotelServiceImpl.java
@@ -0,0 +1,4 @@
+package com.pinming.travelapp.service.impl;
+
+public class HotelServiceImpl {
+}
