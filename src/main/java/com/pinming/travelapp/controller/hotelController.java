eeb184099cfe97b1e75d01200135f292466d7863
zuzu <zuzu_1991@978049543.com>
2019-07-18 20:02:33 +0800

    我打印了一句话

--- src/main/java/com/pinming/travelapp/controller/hotelController.java
+++ src/main/java/com/pinming/travelapp/controller/hotelController.java
@@ -3,7 +3,7 @@ package com.pinming.travelapp.controller;
 public class hotelController {
 
     public static void main(String[] args) {
-
+        System.out.println("9753987934");
         System.out.println("呵呵呵呵");
     }
 }
