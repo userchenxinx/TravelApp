87ec528531365c56062a853f0fc8b14b6d692cfe
xiangxiang <1296933286@qq.com>
2019-07-18 19:25:43 +0800

    添加了一个控制层

---
+++ src/main/java/com/pinming/travelapp/controller/hotelController.java
@@ -0,0 +1,8 @@
+package com.pinming.travelapp.controller;
+
+public class hotelController {
+
+    public static void main(String[] args) {
+        System.out.println("^^^^^^^^^^^^^^^^^^^$##3435435");
+    }
+}
