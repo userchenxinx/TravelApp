6920f50d132f2b561e575680413fc4a4c58a1628
xiangxiang <1296933286@qq.com>
2019-07-18 19:44:01 +0800

    666

--- src/main/java/com/pinming/travelapp/controller/hotelController.java
+++ src/main/java/com/pinming/travelapp/controller/hotelController.java
@@ -3,6 +3,6 @@ package com.pinming.travelapp.controller;
 public class hotelController {
 
     public static void main(String[] args) {
-        System.out.println("^^^^^^^^^^^^^^^^^^^$##3435435");
+
     }
 }
