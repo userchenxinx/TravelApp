260466284f3f9273b5002808358cdaf6b77ff58e
xiangxiang <1296933286@qq.com>
2019-07-18 21:50:02 +0800

    有时间 都在更新一下，，，-朱翔翔

--- src/main/java/com/pinming/travelapp/mapper/HotelMapper.java
+++ src/main/java/com/pinming/travelapp/mapper/HotelMapper.java
@@ -1,4 +1,14 @@
 package com.pinming.travelapp.mapper;
 
+import com.pinming.travelapp.pojo.Hotel;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
 public interface HotelMapper {
+
+    @Select("select * from t_hoteltype_z")
+    List<Hotel> findAllHotel();
 }
